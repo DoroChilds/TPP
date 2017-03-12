8c499fcfde465715f7c952fa76cf39fe085e4e9e
Dorothee Childs <dorothee.childs@embl.de>
2017-03-12 12:59:51 +0000

    Revise the wrapper function that invokes filtering by user-specified QC columns (e.q. 'qssm') for normalization set creation.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/TPP@127325 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/filterTables.R
+++ R/filterTables.R
@@ -1,27 +1,39 @@
 filterTables <- function(data, normReqs){
   ## Filter all data sets by specified criteria on fold changes and metadata columns
-  message("Creating normalization set:")
 
+  message("Creating normalization set:")
+  
   ## Only regard proteins that occur in all treatment groups:
   grNames <- names(data)
-
+  
   message("\t1. Filtering by non fold change columns:")
   filtersOther <- normReqs[["otherRequirements"]]
   cols <- as.character(filtersOther$colName) # Column to filter
   lb   <- filtersOther$thresholdLower # Lower bounds
   ub   <- filtersOther$thresholdUpper # Upper bounds
-
-  fcListFiltered <- sapply(grNames, function(n){
-    message("Filtering by annotation columns ", paste(cols, collapse=" "), 
-            " in treatment group: ", n)
-    filterOther(data=data[[n]], cols=cols, lb=lb, ub=ub)},
-    simplify=FALSE)
-
+  
+  if (length(cols) > 0){
+    
+    fcListFiltered <- sapply(grNames, function(n){
+    
+      longMsg <- paste0("Filtering by annotation column(s) '", 
+                        paste(cols, collapse="', '"), "' in treatment group: ", 
+                        n)
+      
+      message(longMsg)
+      
+      filterOther(data=data[[n]], cols=cols, lb=lb, ub=ub)
+      
+      }, simplify=FALSE)
+    
+  } else fcListFiltered <- data
+  
+  
   ## Determine proteins that occur in each experiment (jointP):
   message("\t2. Find jointP:")
   jointP <- findJointP(data=fcListFiltered)
   fcListJointP   <- sapply(fcListFiltered, function(x){x[jointP,]})
-
+  
   ## Filter by fold changes:
   message("\t3. Filtering fold changes:")
   filtersFC <- normReqs[["fcRequirements"]]
@@ -30,15 +42,18 @@ filterTables <- function(data, normReqs){
   ub  <- filtersFC$thresholdUpper # Upper bounds
   
   fcListFiltered <- sapply(grNames, function(n){
+    
     message("Filtering fold changes in treatment group: ", n)
+    
     filterFCs(data=fcListJointP[[n]], pos=pos, lb=lb, ub=ub)
-  }, simplify=FALSE)
-
+    
+    }, simplify=FALSE)
+  
   ## Find treatment group with largest protein set after filtering (normP):
   protNum    <- sapply(fcListFiltered, nrow)
   grNormP    <- grNames[which.max(protNum)]   # Name of treatment group
   namesNormP <- featureNames(fcListFiltered[[grNormP]]) # Protein IDs in normP
-
+  
   message("Experiment with most remaining proteins after filtering: ", grNormP)
   message("-> NormP contains ", length(namesNormP), " proteins.")
   
