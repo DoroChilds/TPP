248bef801141b8311d280d22943f47005360f3d6
Dorothee Childs <dorothee.childs@embl.de>
2017-03-12 12:58:13 +0000

    Revise the function to filter by user-specified QC columns (e.q. 'qssm') for normalization set creation.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/TPP@127323 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/filterOther.R
+++ R/filterOther.R
@@ -1,6 +1,20 @@
 filterOther <- function(data, cols, lb, ub){
   ## Filter all data sets by specified criteria on metadata columns for 
   ## normalization set construction
+  
+  ## Check if upper and lower bounds are numerics:
+  if (!is.numeric(lb) | !is.numeric(ub)){
+    stop("Normalization requirements must have numeric upper and lower bounds.")
+  }
+  
+  ## Check if the length of the vectors for upper and lower bounds corresponds 
+  ## to the number of desired normalization variables (specified by 'cols'): 
+  if (length(cols) != length(lb) | length(cols) != length(ub)){
+    longMsg <- paste("Upper bounds ('ub') and lower bounds ('lb') must be",
+                      "vectors of the same length as 'cols'.")
+    stop(longMsg)
+  }
+  
   ## Initialize boolean vector indicating valid rows:
   rValid <- rep(TRUE, nrow(data))
   
@@ -8,33 +22,42 @@ filterOther <- function(data, cols, lb, ub){
   datCols <- featureData(data)@data
   
   ## Filter proteins according to lower and upper bounds:
-  for (i in 1:length(cols)){
-    if( cols[i] %in% names(datCols) ){
-      rValTmp <- rep(TRUE, nrow(data))
-      
+  matches <- intersect(cols, names(datCols))
+  nonMatches <- setdiff(cols, names(datCols))
+  if (length(matches) > 0){
+    for (i in 1:length(matches)){
       colsTmp <- cols[i]
       lbTmp   <- lb[i]
       ubTmp   <- ub[i]
       
       x       <- datCols[,colsTmp]
-      lbValid <- x >= lbTmp
-      ubValid <- x <= ubTmp
+      passesLowerBound <- x >= lbTmp
+      passesUpperBound <- x <= ubTmp
       
-      rValTmp[!lbValid]       <- FALSE
-      rValTmp[!ubValid]       <- FALSE
-      rValTmp[is.na(lbValid)] <- FALSE
-      rValTmp[is.na(ubValid)] <- FALSE
-      rValid[rValTmp == FALSE] <- FALSE
+      passesLowerBound[is.na(passesLowerBound)] <- FALSE
+      passesUpperBound[is.na(passesUpperBound)] <- FALSE
       
-      message("  Column ", colsTmp, " between ", lbTmp, " and ", ubTmp, "-> ", sum(rValTmp), " out of ", length(rValTmp), " proteins passed")
+      rValTmp <- passesLowerBound & passesUpperBound
+      rValid[!rValTmp] <- FALSE
       
-    }else{
-      warning(paste( cols[i] , "not found in input columns, can't be used for filtering."))
+      longMsg <- paste0("  Column ", colsTmp, " between ", lbTmp, " and ", 
+                        ubTmp, "-> ", sum(rValTmp), " out of ", length(rValTmp), 
+                        " proteins passed.\n")
+      message(longMsg)
     }
-    
+  }
+  
+  for (nm in nonMatches){
+    longMsg <- paste("Desired column '",nm,"' not found in the data set. ", 
+                 "Therefore, it connot be used as a filter criterion when ", 
+                 "creating normalization set.", 
+                 sep = "")
+    warning(longMsg)
   }
   
   fcFiltered = data[rValid,]
-  message(sum(rValid), " out of ", length(rValid), " proteins passed in total.\n")
+  
+  message(sum(rValid)," out of ",length(rValid)," proteins passed in total.\n")
+  
   return(fcFiltered)
 }
