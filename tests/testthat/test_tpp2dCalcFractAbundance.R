6f36648c8d9dbc6ff6dbc1fb63558c6af6beae62
Dorothee Childs <dorothee.childs@embl.de>
2017-06-06 08:47:58 +0000

    Remove unit tests.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/TPP@130131 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat/test_tpp2dCalcFractAbundance.R
+++
@@ -1,9 +0,0 @@
-# filePath <- system.file("test_data", package="TPP")
-# load(file.path(filePath, "panobinostat_2DTPP_smallExample.RData"))
-# load(system.file("example_data/2D_example_data/fractAbundInData.RData", package="TPP"))
-
-# test_that("evalFractAbund", code={
-#   fracAbund <- tpp2dCalcFractAbundance(data = fractAbundInData)
-#   expect_true(is.numeric(fracAbund$total_sumionarea_fraction))
-#   expect_true(is.numeric(fracAbund$dmso1_vs_dmso2))
-# })
