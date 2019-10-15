adbe0be98a68621380f7a22454a4707b920c59f6
Dorothee Childs <childs@embl.de>
2019-10-15 14:34:57 +0200

    clean up file

--- R/tppDeprecated.R
+++ R/tppDeprecated.R
@@ -1,13 +1,14 @@
 #' @title Deprecated functions in package \sQuote{TPP}
 #' @description These functions are deprecated and no longer available.
 #'  
-#' @details The following function is deprecated; use
-#'  the replacement indicated below:
-#'  \itemize{
-#'  }
+#' %@details The following functions are deprecated; use
+#'%  the replacement indicated below:
+#'%  \itemize{
+#'%  \item
+#'%  }
 #' @export
 #' @return No value returned
 #' 
 #' @name TPP-deprecated
 #' @aliases TPP-deprecated
-
+NULL
